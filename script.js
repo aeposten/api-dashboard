a24bc702f22c393827cad140d6d3ef2976c90a13
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-18 16:35:03 -0800

    Can render stock price

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const CAT_URL = "https://api.thecatapi.com/v1/images/search";
 const DAD_JOKE_URL = "https://dad-jokes.p.rapidapi.com/joke/type/programming";
+const FIN_URL = "https://finnhub.io/api/v1/";
+
+const FIN_KEY = "cf48klaad3i94dsnolugcf48klaad3i94dsnolv0"
 
 const setupEl = document.getElementById("setup");
 const punchlineEl = document.getElementById("punchline");
+const stockPrice = document.getElementById("stock-price")
 
 async function setBodyBackground() {
-  const response = await fetch(`${CAT_URL}?breed_id=bsho`);
-  const data = await response.json();
-
-  const imageURL = data[0].url;
-
-  document.body.style.backgroundImage = `url(${imageURL})`;
+  try {
+    const response = await fetch(`${CAT_URL}?breed_id=bsho`);
+    const data = await response.json();
+    const imageURL = data[0].url;
+    document.body.style.backgroundImage = `url(${imageURL})`;
+  } catch (error) {
+    console.error;
+  }
 }
 
 async function setDadJoke() {
@@ -21,17 +27,34 @@ async function setDadJoke() {
       "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com",
     },
   };
-  const response = await fetch(`${DAD_JOKE_URL}`, options);
-  const data = await response.json();
 
-  const randomJoke = data.body[Math.floor(Math.random() * (18 - 0) + 0)];
+  try {
+    const response = await fetch(`${DAD_JOKE_URL}`, options);
+    const data = await response.json();
 
-  const setupTxt = randomJoke.setup;
-  const punchlineTxt = randomJoke.punchline;
+    const randomJoke = data.body[Math.floor(Math.random() * (18 - 0) + 0)];
 
-  setupEl.textContent = setupTxt;
-  punchlineEl.textContent = punchlineTxt;
+    const setupTxt = randomJoke.setup;
+    const punchlineTxt = randomJoke.punchline;
+
+    setupEl.textContent = setupTxt;
+    punchlineEl.textContent = punchlineTxt;
+  } catch (error) {
+    console.error;
+  }
 }
 
+async function fetchStockPrice() {
+
+  const response = await fetch(
+    `${FIN_URL}quote?symbol=AAPL&token=${FIN_KEY}`
+  );
+  const data = await response.json();
+  const currentPrice = data.c
+
+  stockPrice.textContent = `APPL: $ ${currentPrice}`;
+}
+
+fetchStockPrice();
 setBodyBackground();
 setDadJoke();
