dc3ba35d235d0ca56029657f31fdfb9b6f9f8e36
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-19 09:19:49 -0800

    Current time displays in center of screen

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const CAT_URL = "https://api.thecatapi.com/v1/images/search";
 const DAD_JOKE_URL = "https://dad-jokes.p.rapidapi.com/joke/type/programming";
 const FIN_URL = "https://finnhub.io/api/v1/";
 
-const FIN_KEY = "cf48klaad3i94dsnolugcf48klaad3i94dsnolv0"
+const FIN_KEY = "cf48klaad3i94dsnolugcf48klaad3i94dsnolv0";
 
 const setupEl = document.getElementById("setup");
 const punchlineEl = document.getElementById("punchline");
-const stockPrice = document.getElementById("stock-price")
+const stockPrice = document.getElementById("stock-price");
+
+const timeEl = document.getElementById("current-time");
 
 async function setBodyBackground() {
   try {
@@ -15,7 +17,7 @@ async function setBodyBackground() {
     const imageURL = data[0].url;
     document.body.style.backgroundImage = `url(${imageURL})`;
   } catch (error) {
-    console.error;
+    console.error(error);
   }
 }
 
@@ -40,21 +42,27 @@ async function setDadJoke() {
     setupEl.textContent = setupTxt;
     punchlineEl.textContent = punchlineTxt;
   } catch (error) {
-    console.error;
+    console.error(error);
   }
 }
 
 async function fetchStockPrice() {
-
-  const response = await fetch(
-    `${FIN_URL}quote?symbol=AAPL&token=${FIN_KEY}`
-  );
+  const response = await fetch(`${FIN_URL}quote?symbol=AAPL&token=${FIN_KEY}`);
   const data = await response.json();
-  const currentPrice = data.c
+  const currentPrice = data.c;
 
   stockPrice.textContent = `APPL: $ ${currentPrice}`;
 }
 
+function setCurrentTime() {
+  const date = new Date();
+  const time = date.toLocaleTimeString();
+
+  timeEl.textContent = time;
+  setTimeout(setCurrentTime, 1000);
+}
+
+setCurrentTime();
 fetchStockPrice();
 setBodyBackground();
 setDadJoke();
